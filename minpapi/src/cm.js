16180040ada746679998abc6d04df76197e28a11
2017zhaoxiaoxiao <1877145940@qq.com>
2019-10-20 14:43:16 +0800

    add cm.js

--- minpapi/src/cm.js
+++ minpapi/src/cm.js
@@ -13,6 +13,29 @@ var menu_data={
             url : "https://www.linux.org"
         },
         {
+            name : "发图",
+            "sub_button": [
+                {
+                    "type": "pic_sysphoto", 
+                    "name": "系统拍照发图", 
+                    "key": "rselfmenu_1_0", 
+                   "sub_button": [ ]
+                 }, 
+                {
+                    "type": "pic_photo_or_album", 
+                    "name": "拍照或者相册发图", 
+                    "key": "rselfmenu_1_1", 
+                    "sub_button": [ ]
+                }, 
+                {
+                    "type": "pic_weixin", 
+                    "name": "微信相册发图", 
+                    "key": "rselfmenu_1_2", 
+                    "sub_button": [ ]
+                }
+            ]
+        },
+        {
             name : "help",
             type : "click",
             key : "send-msg"
